2b32b08ac5fd840608a17eda692d7ed26e12efd7
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-04-26 20:27:11 -0300

    Removido comentarios desnecessários

--- Barbearia/src/barbearia/Barbearia.java
+++ Barbearia/src/barbearia/Barbearia.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package barbearia;
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-/**
- *
- * @author CaueRyan
- */
+
 public class Barbearia implements Runnable{
     private String threadName;
     
